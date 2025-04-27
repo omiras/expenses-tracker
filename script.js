be5c23f3d2f9afad9d441c69868246ab2bd318a4
omiras <oscar.miras@gmail.com>
2025-04-27 11:48:37 +0200

    done 1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Happy coding
+const fs = require("fs");
+
+
+
+const parsedData = fs.readFileSync("expenses.json", "utf-8");
+const expenses = JSON.parse(parsedData);
+
+const command = process.argv[2];
+
+if (command == "--list") {
+    console.log("List of expenses: ID Date Concept Category Amount");
+    console.log("-------------------------------------------------");
+     for (const e of expenses) {
+        console.log(`#${e.id} ${new Date(e.timestamp * 1000).toLocaleDateString()} ${e.concept} ${e.category} ${e.amount} €`);
+    }
+}
