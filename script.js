adf2ea2f7376943c03a8ae0552c1ddf729502e40
omiras <oscar.miras@gmail.com>
2025-04-27 11:49:31 +0200

    done

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const expenses = JSON.parse(parsedData);
 const command = process.argv[2];
 
 if (command == "--list") {
-    console.log("List of expenses: ID Date Concept Category Amount");
+    console.log("ID Date Concept Category Amount");
     console.log("-------------------------------------------------");
      for (const e of expenses) {
         console.log(`#${e.id} ${new Date(e.timestamp * 1000).toLocaleDateString()} ${e.concept} ${e.category} ${e.amount} €`);
