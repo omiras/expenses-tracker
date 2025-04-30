e2cdef9f52da9df94fa38d7ec6cead27d452f9ca
omiras <oscar.miras@gmail.com>
2025-04-30 18:49:22 +0200

    done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // Happy coding
 
 // Primeros pasos: carga el fichero JSON y muestra su contenido en la consola. Luego, ya puedes implementar la iteración 1. Para mostrar la fecha legible, puedes buscar por Chat GPT o por Google como convertir un timestamp 
+
+// Corregir 19h: Cargar el JSON y mostrar cada uno de los objetos (sin procesar, en crudo)
